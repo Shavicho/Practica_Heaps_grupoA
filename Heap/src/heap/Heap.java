09088b6e5e607f709607847bc7ef8848c792ec84
Diego Nina <dninas@unsa.edu.pe>
2023-06-17 02:47:05 -0500

    Heap and Updating HeapInterface

--- Heap/src/heap/Heap.java
+++ Heap/src/heap/Heap.java
@@ -19,41 +19,68 @@ public class Heap<T extends Comparable<T>> implements HeapInterface<T>{
         if (heap.isEmpty()) {
             throw new Exception("El heap esta vacio");
         }
-
         T maximo = heap.get(0);
         T ultimoElemento = heap.remove(heap.size() - 1);
-
+        
         if (!heap.isEmpty()) {
             heap.set(0, ultimoElemento);
             hundir(0); // debe undir el elemento 
         }
-
         return maximo;
     }
 
     @Override
-    public T obtener() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public T obtenerMaximo()  throws Exception {
+        if (heap.isEmpty()) {
+            throw new Exception("El heap esta vacio");
+        }
+        return heap.get(0);
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return heap.isEmpty();
     }
 
-    @Override
-    public void flotar(int indice) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private void flotar(int indice) {
+        T elemento = heap.get(indice);
+        int indicePadre = getIndicePadre(indice);//devolver el indice padre
+
+        while (indice > 0 && elemento.compareTo(heap.get(indicePadre)) > 0) {
+            heap.set(indice, heap.get(indicePadre));
+            indice = indicePadre;
+            indicePadre = getIndicePadre(indice);//devolver el indice padre
+        }
+        heap.set(indice, elemento);
     }
 
-    @Override
-    public void hundir(int indice) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private void hundir(int indice) {
+        T elemento = heap.get(indice);
+        int indiceHijoIzquierdo = getIndiceHijoIzquierdo(indice);//devolver el indice de hijo izquierdp
+
+        while (indiceHijoIzquierdo < heap.size()) {
+            if (indiceHijoIzquierdo + 1 < heap.size() && heap.get(indiceHijoIzquierdo + 1).compareTo(heap.get(indiceHijoIzquierdo)) > 0) {
+                indiceHijoIzquierdo++;
+            }
+
+            if (elemento.compareTo(heap.get(indiceHijoIzquierdo)) >= 0) {
+                break;
+            }
+
+            heap.set(indice, heap.get(indiceHijoIzquierdo));
+            indice = indiceHijoIzquierdo;
+            indiceHijoIzquierdo = getIndiceHijoIzquierdo(indice);//devolver el indice de hijo izquierdp
+        }
+
+        heap.set(indice, elemento);
+    }
+    
+    private int getIndicePadre(int indice) {
+        return (indice - 1) / 2;
     }
 
-    @Override
-    public int getIndicePadre(int indice) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public int getHijoIzquierdo(int indice) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private int getIndiceHijoIzquierdo(int indice) {
+        return 2 * indice + 1;
     }
     
     
--- Heap/src/heap/HeapInterface.java
+++ Heap/src/heap/HeapInterface.java
@@ -3,9 +3,6 @@ package heap;
 public interface HeapInterface<T> {
     void insertar(T data);
     T eliminarMaximo() throws Exception;
-    T obtener();
-    void flotar(int indice);
-    void hundir(int indice);
-    int getIndicePadre(int indice);
-    int getHijoIzquierdo(int indice);
+    T obtenerMaximo() throws Exception;
+    boolean isEmpty();
 }
